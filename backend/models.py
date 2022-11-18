6aff50888d4f1c39d5a9f86dfe5593e7ce3ba9ef
unknown <yousalb891@student.polsl.pl>
2022-11-18 20:59:54 +0100

    'not_working'

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    login = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader, RequestContext
+from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_exempt, csrf_protect, requires_csrf_token
 
-# Create your views here.
+from .models import User
+
+
+
+
+
+@requires_csrf_token
+def profile(request):
+    if request.method == "POST":
+        data = request.POST
+        user = User()
+        user.login = data.get("email")
+        user.password = data.get("psw")
+        user.save()
+    return render("index.html",  RequestContext(request))
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
