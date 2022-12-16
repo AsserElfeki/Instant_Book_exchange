3a2c3d828b9bfd77d57b1f4ca3bba33dcd232e7c
Asser <66274223+AsserElfeki@users.noreply.github.com>
2022-12-16 20:42:09 +0100

    sign in template

--- Boookz-Nuxt/pages/signin.vue
+++ Boookz-Nuxt/pages/signin.vue
@@ -1,16 +1,49 @@
 <template>
-  <div class="container">
-    <div class="img">
+  <div class=" flex mt-32 mx-auto  justify-center items-center gap-40 ">
+    <div class="img ">
       <img src="../assets/img/sign-in-img.png" alt="photo of a lady looking from her window">
     </div>
-    <div class="content">
-      <h1>Hiii </h1>
+    <div class=" ">
+      <form action="submit" class="flex flex-col justify-between gap-4" >
+        <h1 class="text-3xl font-black ">Sign in</h1>
+        <div class="">
+          <label for="email"></label>
+          <input class="input" size="28" type="email" name="email" id="email" placeholder="Enter your email">
+        </div>
+        <div class="">
+          <label for="password"></label>
+          <input class="input" size="28" type="password" name="password" id="password" placeholder="Enter your password">
+        </div>
+        <div class="">
+          <input type="checkbox" name="remember" id="remember" >
+          <label for="remember"> Remember me</label>
+        </div>
+        <div class="">
+          <button type="submit" class="btn" @click="signIn">Sign in</button>
+        </div>
+        <div class="text-blue-300 border border-transparent rounder hover:border hover:rounded hover:shadow-md mx-auto px-2 ">
+          <p><NuxtLink to="/register">Don't have an account? Sign up</NuxtLink></p>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useStore } from '~/stores/store'
+const store = useStore()
 
-<style lang="scss" scoped>
+const signIn = () => {
 
+  store.signIn(userName, userPassword)
+}
+
+</script>
+
+<style scoped>
+.input{
+  border: 2px solid black;
+  border-radius: 7px;
+  padding: 10px;
+}
 </style>
--- Boookz-Nuxt/stores/store.js
+++ Boookz-Nuxt/stores/store.js
@@ -4,6 +4,8 @@ export const useStore = defineStore({
   id: 'store',
   state: () => ({
     books: [],
+    userName: '',
+    userPassword: '',
     userIsSearching: false,
     userIsLoggedIn: false,
   }),
@@ -14,10 +16,16 @@ export const useStore = defineStore({
       this.books = data;
       // console.log(this.books);
     },
+    async signIng(userName, userPassword) {
+      this.userIsLoggedIn = true;
+      //ToDO 2
+    },
   },
 
   //to get specific parts of data, like select <items> from <container> WHERE <condition>
   getters: {},
 });
 
-// ToDo user and token in data ?
+// ToDo :
+//1. user and token in data ?
+//2. Post request to backend with user name and password
