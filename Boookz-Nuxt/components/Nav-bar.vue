168bc4f2dbf15f25b5399ab8a167cd45d234f703
Asser Elfeki <66274223+AsserElfeki@users.noreply.github.com>
2022-12-16 18:54:48 +0100

    app name and profile link

--- Boookz-Nuxt/components/Nav-bar.vue
+++ Boookz-Nuxt/components/Nav-bar.vue
@@ -1,8 +1,12 @@
 <template>
 
   <div class="navBar">
-    <NuxtLink to="/home">
+    <NuxtLink to="/home" class="flex gap-2">
       <AppLogo />
+      <div class="app-name">
+        <h1 class="hi">Boookz</h1>
+        <p class="text-xs">Book xchange website</p>
+      </div>
     </NuxtLink>
     <div class="search-box">
       <input placeholder="Search" type="text" class="search-bar" v-model="searchQuery">
@@ -10,12 +14,15 @@
         <img src="/MagnifyingGlass.png" alt="search icon" class="absolute top-0 right-2 ">
       </button>
     </div>
-    <ul class="flex flex-row ">
-      <li class="mr-7 btn">
+    <ul class="flex flex-row gap-2">
+      <li class=" btn">
         <NuxtLink to="/signin">Sign in</NuxtLink>
       </li>
-      <li class="btn">
+      <li class=" btn">
         <NuxtLink to="/register">Sign up</NuxtLink>
+      </li>
+      <li class="btn">
+        <NuxtLink to="/profile">Profile</NuxtLink>
       </li>
     </ul>
   </div>
@@ -41,8 +48,21 @@ export default {
 
 </script>
 
-<style  scoped>
+<style scoped>
+.appname h1 {
+  font-size: 3.5rem;
+  font-weight: 100;
+}
 
+.hi {
+  width: 117px;
+  height: 35px;
+  font-family: 'Segoe UI';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 33px;
+  line-height: 44px;
+}
 </style>
 
 // ToDO : 
@@ -51,3 +71,4 @@ export default {
 // 3. Add a search function ✅
 // 6. Add a search result component to the home page
 // 7. get authentication status and display nav bar accordingly
+
