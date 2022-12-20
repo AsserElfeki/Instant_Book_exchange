c5737d84910d6c090f7df20e92e06341dd017c3b
Asser <66274223+AsserElfeki@users.noreply.github.com>
2022-12-20 22:16:37 +0100

    redirect trials

--- Boookz-Nuxt/ToDos.md
+++ Boookz-Nuxt/ToDos.md
@@ -15,15 +15,30 @@
 - [ ] 14. random book styles
 - [x] 15. solve token issue in store
 - [x] 16. split data store from user store (asser)
-- [ ] 17. fix the post request to send the token also (as header) check old frontend (asser)
-- [ ] 18. figure out how to redirect after login and data refresh and kinda data save !?
+- [x] 17. fix the post request to send the token also (as header) check old frontend (asser)
+- [x] 18. figure out how to redirect after login and data refresh and kinda data save !?
 - [x] 19. figure out how to save session (I am not a front end dev anymore. FM)
 - [ ] 20. figure out a "loading state" importance : 3
-- [ ] **21. register form (kinda done) but with double password check and (username, first, last, email)**
-- [ ] 22. @change of the username field I send to an endpoint that tells me if this user name is taken or not 
+- [x] 21. register form (kinda done) but with double password check and (username, first, last, email)
+- [x] 22. @change of the username field I send to an endpoint that tells me if this user name is taken or not 
 - OR display response in a Toast
 - [x] 23. sign out
+- [x] 24. in register: res.response : happy 
+                       res.username : username is taken
+                          res.email : email is taken
+                          res.password: weak 
+                          res.password2: no match
+- [ ] 25. Toats for last task
+- [ ] 26. redirect after register
+- [ ] 27. redirect after sign in
+- [x] 28. logout to endpoint 
 
+
+
+###will DO: 
+25, 26, 27
+profile page template 
+homepage data fetching 
 
 ## Parts to do : 
 - [ ] 1. urls of post request in store. 
--- Boookz-Nuxt/stores/userStore.js
+++ Boookz-Nuxt/stores/userStore.js
@@ -22,6 +22,8 @@ export const useUserStore = defineStore({
                 this.token = res.access;
                 if (this.token) {
                     this.userIsLoggedIn = true;
+                    await navigateTo('/')
+
                 }
             }
             catch (error) {
@@ -32,10 +34,27 @@ export const useUserStore = defineStore({
 
         async register(form) {
             try {
-                return await $fetch('http://localhost:4000/users', {
+                const res = await $fetch('http://localhost:8000/authentication/register/', {
                     method: 'POST',
                     body: form
                 })
+                if (res.response) {
+                    //display a toast that says register successful 
+                }
+                else {
+                    if (res.username) {
+                        //display a toast that says username already exists
+                    }
+                    else if (res.email) {
+                        //display a toast that says email already exists
+                    }
+                    else if (res.password) {
+                        //display a toast that says password is too short
+                    }
+                    else if (res.password2) {
+                        //display a toast that says passwords do not match
+                    }
+                }
             }
             catch (error) {
                 console.log(error);
@@ -43,7 +62,11 @@ export const useUserStore = defineStore({
 
         },
 
-        logOut() {
+        async logOut() {
+            const res = await $fetch('http://localhost:8000/authentication/logout_all/', {
+                method: 'POST',
+                headers: { "authorization": "Bearer " + this.token }
+            })
             this.userIsLoggedIn = false;
             this.token = '';
         }
