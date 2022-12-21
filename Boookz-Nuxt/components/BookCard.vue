28fb238d55f3b3a9d562f7f7d538b207900982b2
unknown <yousalb891@student.polsl.pl>
2022-12-21 16:00:43 +0100

    Added profile pic to the card

--- Boookz-Nuxt/components/BookCard.vue
+++ Boookz-Nuxt/components/BookCard.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="book-card flex items-center justify-between">
-        <div class="overflow-hidden">
+    <div class="book-card flex items-center justify-between overflow-visible">
+        <div class="overflow-visible">
             <img :src="book.image" :alt="book.title" class="cover">
+            <img :src="book.image" :alt="book.title" class="user-image w-16 h-16 rounded-full object-cover absolute left-1/2">
             <p class="text-[#e76f51] text-2xl h-20">{{ book.title }}</p>
             <p class="text-secondary text-lg my-3">{{ book.points }} point</p>
             <p>Offered by: {{ book.owner }}</p>
@@ -22,12 +23,18 @@ const { book } = defineProps(['book'])
     background-color: rgb(221,218,238); /* set the background color */
 }
 .book-card {
-
+    position: relative;
     margin: 1rem;
     padding: 1rem;
     border: 2px solid black;
     border-radius: 20px;
     box-shadow: 0 0 10px rgba(0,0,0,0.5);
-    background-color:rgb(221,218,238); /* set the background color */
+    background-color:rgb(221,218,238); 
+    overflow: visible;	
+}
+
+.user-image {
+top: 450px; 
+transform: translateX(-50%);
 }
 </style>
