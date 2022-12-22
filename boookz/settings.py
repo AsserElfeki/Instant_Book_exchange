16969297327e6917685033087cfee4e38b925294
claude-perrin <vity5.diduk@gmail.com>
2022-12-22 12:03:19 +0100

    access token is valid for 45 min

--- boookz/settings.py
+++ boookz/settings.py
@@ -67,6 +67,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=45),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION':False,
