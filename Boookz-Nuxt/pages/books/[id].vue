f45e719b8e013e3799dbf47b8d9157681887dbde
Asser <66274223+AsserElfeki@users.noreply.github.com>
2022-12-28 14:23:04 +0100

    solved the single root issue in [id]

--- Boookz-Nuxt/pages/books/[id].vue
+++ Boookz-Nuxt/pages/books/[id].vue
@@ -1,65 +1,62 @@
 <template>
-    <swiper
-    :slides-per-view="3"
-    :space-between="50"
-    @swiper="onSwiper"
-    @slideChange="onSlideChange"
-  >
-  >
-    <swiper-slide>
-        <img class="rounded-md" src="../../assets/img/book-cover.jpg" alt="cover of the book">
-    </swiper-slide>
-    <swiper-slide>
-        <img class="rounded-md " src="../../assets/img/book-cover.jpg" alt="cover of the book">
-    </swiper-slide>
-    <swiper-slide>
-        <img class="rounded-md " src="../../assets/img/book-cover.jpg" alt="cover of the book">
-    </swiper-slide>
-    <swiper-slide>
-        <img class="rounded-md " src="../../assets/img/book-cover.jpg" alt="cover of the book">
-    </swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    ...
-  </swiper>
-  <div class="book-display flex items-center flex-wrap">
-    <div class="book-container flex items-center">
-      <img class="book-cover rounded-md w-48 h-64 mr-5" src="../../assets/img/book-cover.jpg" alt="cover of the book">
-      <div class="book-info flex-1">
-      <h2 class="book-title">{{ store.clickedBook.title }}</h2>
-      <p class="book-condition">Owner: {{ store.clickedBook.book_owner }}</p>
-      <p class="book-condition">Condition: {{ store.clickedBook.condition }}</p>
-      <p class="book-condition">Category: {{ store.clickedBook.category }}</p>
+  <div class="container">
+    <swiper :slides-per-view="3" :space-between="50" @swiper="onSwiper" @slideChange="onSlideChange">
+      >
+      <swiper-slide>
+        <img class="rounded-md" src="../../assets/img/book-cover.jpg" alt="cover of the book" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="rounded-md" src="../../assets/img/book-cover.jpg" alt="cover of the book" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="rounded-md" src="../../assets/img/book-cover.jpg" alt="cover of the book" />
+      </swiper-slide>
+      <swiper-slide>
+        <img class="rounded-md" src="../../assets/img/book-cover.jpg" alt="cover of the book" />
+      </swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      ...
+    </swiper>
+    <div class="book-display flex items-center flex-wrap">
+      <div class="book-container flex items-center">
+        <img class="book-cover rounded-md w-48 h-64 mr-5" src="../../assets/img/book-cover.jpg"
+          alt="cover of the book" />
+        <div class="book-info flex-1">
+          <h2 class="book-title">{{ store.clickedBook.title }}</h2>
+          <p class="book-condition">Owner: {{ store.clickedBook.book_owner }}</p>
+          <p class="book-condition">Condition: {{ store.clickedBook.condition }}</p>
+          <p class="book-condition">Category: {{ store.clickedBook.category }}</p>
+        </div>
+        <button class="btn">Trade</button>
       </div>
-      <button class="btn" >Trade</button>
     </div>
-    </div>
+  </div>
 </template>
 
-
 <script>
-import { useDataStore } from '~/stores/dataStore'
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import 'swiper/css';
-const store = useDataStore()
+import { useDataStore } from "~/stores/dataStore";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+const store = useDataStore();
 export default {
-components: {
+  components: {
     Swiper,
     SwiperSlide,
-},
+  },
 
-setup() {
+  setup() {
     const onSwiper = (swiper) => {
-    console.log(swiper);
+      console.log(swiper);
     };
     const onSlideChange = () => {
-    console.log('slide change');
+      console.log("slide change");
     };
     return {
-    onSwiper,
-    onSlideChange,
-    store,
+      onSwiper,
+      onSlideChange,
+      store,
     };
-},
+  },
 };
 </script>
 
