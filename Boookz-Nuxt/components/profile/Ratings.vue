eb003c04fe65c1c0cbbc719171df1df4c062581b
sshch <s.shcherban00@gmail.com>
2022-12-30 15:46:22 +0200

    transaction and reeding section

--- Boookz-Nuxt/components/profile/Ratings.vue
+++ Boookz-Nuxt/components/profile/Ratings.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
-        <p>Hi from GiveAway Ratings</p>
+        <p><strong>My Reviews</strong></p>
 
     </div>
+    <div class="box">
+        <img src="../../assets/img/avatar (1).png" alt="profile avatar" class="Avatar" />
+        <h2 class="Fat Tony"><br>&nbsp;Leonardo is a good man, I like him!<br>&nbsp;April 14th, 2022</h2>
+        <h2 class="Descrypt">&nbsp;<strong>Fat Tony</strong> rated whith 5 stars</h2>
+    </div>
+    <div class="boxx">
+        <img src="../../assets/img/avatar (1).png" alt="profile avatar" class="Avatar" />
+        <h2 class="Ben Franklin"><br>&nbsp;I am not really satisfied with Leonardo, he sent me a box of c****** instead of my desired book, but as a man I am still grateful. Thank you Leonardo.<br>&nbsp;June 24th, 2022</h2>
+        <h2 class="Descryptt">&nbsp;<strong>Ben Franklin</strong> rated whith 5 stars</h2>
+    </div>
+
 </template>
 
 <script setup>
@@ -10,5 +21,90 @@
 </script>
 
 <style scoped>
+.box {
+    box-sizing: border-box;
+    position: absolute;
+    width: 1205px;
+    height: 180px;
+    left: 159px;
+    top: 687px;
+    background: #FFFFFF;
+    border: 1px dashed #000000;
+    border-radius: 40px;
+}
+.Avatar {
+    max-width: 50%;
+    height: 30%;
+    position: static;
+    margin-left: 1.32%;
+    margin-top: 1%;
+
+}
+.Fat Tony {
+    position: static;
+    width: 119px;
+    height: 40px;
+    left: 259px;
+    top: 949px;
+    font-family: 'Segoe UI';
+    font-style: bold;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 40px;
+    color: #04040D;
+}
+.Descrypt {
+    position: static;
+    width: 229px;
+    height: 40px;
+    margin-left: 79px;
+    margin-top: -119px;
+    font-family: 'Segoe UI';
+    font-style: bold;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 40px;
+    color: #04040D;
+}
+.Descryptt {
+    position: static;
+    width: 239px;
+    height: 40px;
+    margin-left: 79px;
+    margin-top: -119px;
+    font-family: 'Segoe UI';
+    font-style: bold;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 40px;
+    color: #04040D;
+}
+.Ben Franklin {
+    position: static;
+    width: 119px;
+    height: 40px;
+    left: 259px;
+    top: 949px;
+    font-family: 'Segoe UI';
+    font-style: bold;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 40px;
+    color: #04040D;
+}
+.boxx {
+    box-sizing: border-box;
+    position: absolute;
+    width: 1205px;
+    height: 180px;
+    left: 159px;
+    top: 920px;
+    background: #FFFFFF;
+    border: 1px dashed #000000;
+    border-radius: 40px;
+}
+
+
+
 
 </style>
