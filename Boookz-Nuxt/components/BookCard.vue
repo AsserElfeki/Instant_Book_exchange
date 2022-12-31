364c45a3d65510a93eae11dd5298c14ac5c5c0d4
Asser <66274223+AsserElfeki@users.noreply.github.com>
2022-12-31 20:52:15 +0100

    transactions responsive

--- Boookz-Nuxt/components/BookCard.vue
+++ Boookz-Nuxt/components/BookCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="book-card my-4 max-w-xs shadow-xl  relative rounded-3xl flex flex-col items-center overflow-visible p-3">
+        class="bg-[#dddaee] my-4 max-w-xs shadow-xl  relative rounded-3xl flex flex-col items-center overflow-visible p-3">
         <div class="rounded-3xl w-full max-h-[800px]">
             <img :src="book.images.at(0)" :alt="book.title" class="cover rounded-3xl w-full max-h-40 object-contain">
 
@@ -10,7 +10,7 @@
             </div>
         </div>
         <img :src="book.book_owner.at(1)" :alt="book.title"
-            class="user-image w-16 h-16 rounded-full object-cover absolute left-1/2 border-black border">
+            class="user-image w-16 h-16 rounded-full object-cover absolute bottom-[-40px] left-1/2 -translate-x-1/2 border-black border">
     </div>
 </template>
 
@@ -23,29 +23,3 @@ function truncate(string, value) {
         return string;
 }
 </script>
-
-<style  scoped>
-.covere {
-    /* width: 20rem; */
-    /* height: 15rem; */
-    object-fit: contain;
-    background-color: rgb(221, 218, 238);
-    /* set the background color */
-}
-
-.book-card {
-    /* position: relative; */
-    /* margin: 1rem;
-    padding: 1rem; */
-    /* border: 2px solid black; */
-    /* border-radius: 20px; */
-    /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); */
-    background-color: rgb(221, 218, 238);
-    overflow: visible;
-}
-
-.user-image {
-    bottom: -40px;
-    transform: translateX(-50%);
-}
-</style>
--- Boookz-Nuxt/components/profile/Transactions.vue
+++ Boookz-Nuxt/components/profile/Transactions.vue
@@ -1,22 +1,37 @@
 <template>
-  <div class="container">
-    <div v-for="transaction in store.userTransactions">
+  <div class="flex flex-col gap-4 justify-center">
+  <h2 class="font-bold text-xl font-sans mx-2">My History</h2>
+    <div
+      v-for="transaction in store.userTransactions"
+      :key="transaction.id"
+      class="flex flex-col gap-2 m-2"
+    >
       <h2>Date: {{ transaction.date }}</h2>
-        <h2>Transaction with: {{ transaction.book_owner.at(0) }}</h2>
+      <h2>Transaction with: {{ transaction.book_owner.at(0) }}</h2>
       <div class="bg-[#F2F0FE] rounded-xl p-4 flex flex-col gap-2">
-        <div class="flex justify-between bg-white border border-dashed border-black rounded-xl p-4">
-          <h3>{{ transaction.title }}</h3>
+        <div
+          class="flex flex-col md:flex-row justify-between bg-white border border-dashed border-black rounded-xl p-4 gap-2 items-center"
+        >
+          <h3 class="font-bold text-lg font-segoe">{{ transaction.title }}</h3>
           <h3>{{ transaction.author.at() }}</h3>
-       <font-awesome-icon icon="fa-solid fa-arrow-right-from-bracket" 
-       class="text-green-500 fa-xl"/>
+          <font-awesome-icon
+            icon="fa-solid fa-arrow-right-from-bracket"
+            class="text-green-500 fa-xl"
+          />
         </div>
-        <div class="flex justify-between bg-white border border-dashed border-black rounded-xl p-4">
-          <h3>{{ transaction.title }}</h3>
+        <div
+          class="flex flex-col md:flex-row justify-between bg-white border border-dashed border-black rounded-xl p-4 gap-2 items-center"
+        >
+          <h3 class="font-bold text-lg font-segoe" >{{ transaction.title }}</h3>
           <h3>{{ transaction.author.at() }}</h3>
-          <font-awesome-icon icon="fa-solid fa-arrow-right-from-bracket"   class="rotate-180 text-red-500 fa-xl"/>
+          <font-awesome-icon
+            icon="fa-solid fa-arrow-right-from-bracket"
+            class="rotate-180 text-red-500 fa-xl"
+          />
         </div>
       </div>
     </div>
+    
   </div>
 </template>
 
@@ -26,4 +41,6 @@ const store = useUserStore();
 store.getUserHistory();
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
