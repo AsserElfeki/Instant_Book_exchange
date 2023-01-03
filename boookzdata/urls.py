c16333bf23de591df815b234bee72305758ac7e4
drago20013 <smaluchmichal@gmail.com>
2023-01-03 16:19:36 +0100

    bookupload v2

--- boookzdata/urls.py
+++ boookzdata/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from .views import AllGiveawayView, BookViewSet, ImageViewSet, GiveawayBookUploadView, WantedBookUploadView, BooksFromChosenBookshelfView, SearchGiveAwayBooksView, AllWantedView, AllGiveawayView
+from .views import AllGiveawayView, BookViewSet, ImageViewSet, BookUploadView, BooksFromChosenBookshelfView, SearchGiveAwayBooksView, AllWantedView, AllGiveawayView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -9,8 +9,7 @@ router.register('image', ImageViewSet, basename='Image')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('upload/giveaway', GiveawayBookUploadView.as_view(), name='uploadGiveaway'),
-    path('upload/wanted', WantedBookUploadView.as_view(), name='uploadWanted'),
+    path('upload/<str:bookshelf_param>', BookUploadView.as_view(), name='upload'),
     path('shelf/<str:bookshelf>', BooksFromChosenBookshelfView.as_view(), name='GiveawayBookshelf'),
     path('wanted/', AllWantedView.as_view(), name='WantedBooks'),
     path('giveaway/', AllGiveawayView.as_view(), name='GiveawayBooks'),
