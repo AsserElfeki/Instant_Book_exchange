fb821f4749246552591e465ad554998cbe7e2ac5
drago20013 <smaluchmichal@gmail.com>
2023-01-05 00:56:19 +0100

    transaction history added to profile endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 
 from authentication.views import ProfileInfoView, RegisterView, ChangePasswordView, UpdateProfileView, LogoutView, \
-    LogoutAllView, ListBookReaderBooks, EmailVerify, LoginView, DeleteAccount
+    LogoutAllView, ListBookReaderBooks, EmailVerify, LoginView, DeleteAccount, AnyProfileInfoView
 from rest_framework_simplejwt.views import TokenRefreshView 
-
-from rest_framework.routers import Route, DynamicRoute, SimpleRouter
 
 urlpatterns = [
     path('list/', ListBookReaderBooks.as_view({'get':'list'}), name='list_book_reader'),
@@ -16,6 +14,7 @@ urlpatterns = [
     path('update_profile/<int:pk>/', UpdateProfileView.as_view(), name='auth_update_profile'),
     path('delete/', DeleteAccount.as_view(), name="profile_delete"),
     path('profile/', ProfileInfoView.as_view({'get': 'list'}), name="profile_info"),
+    path('profile/<str:username>', AnyProfileInfoView.as_view(), name="any_profile_info"),
     path('logout/', LogoutView.as_view(), name='auth_logout'),
     path('logout_all/', LogoutAllView.as_view(), name='auth_logout_all'),
 ]
