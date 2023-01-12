40fab931daff9cdb400684e28fb1725250e195e9
Michal Smaluch <smaluchmichal@gmail.com>
2023-01-12 11:06:31 +0100

    fixed route guard

--- Boookz-Nuxt/middleware/auth.ts
+++ Boookz-Nuxt/middleware/auth.ts
@@ -2,13 +2,12 @@ import { useUserStore } from '~/stores/userStore'
 const store = useUserStore()
 const isAuthenticated = store.userIsLoggedIn
 
-console.log(isAuthenticated)
 export default defineNuxtRouteMiddleware(() => {
     // isAuthenticated() is an example method verifying if an user is authenticated
     // console.log("state: " + isAuthenticated)
     // console.log({ to, from })
     if (process.client && isAuthenticated === false) {
-        return navigateTo('signIn')
+        return navigateTo('/signIn')
     }
     
 })
